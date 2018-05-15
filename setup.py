7332190499c3a7ad55134c3ce31d9686312f34ae
Dennis Muth <dennis.muth@magicline.de>
2018-05-15 22:19:19 +0200

    Fixes argresolver version number
    The version of argresolver was accidentially overwritten by the
    bumpversion tool. We need to find a solution for that but first
    fix the issue

--- setup.py
+++ setup.py
@@ -53,10 +53,10 @@ setup(
         'requests>=2.18.4',
         'schedule>=0.5.0',
         'schema>=0.6.7',
-        'argresolver==0.3.0'
+        'argresolver==0.2.0'
     ],
     dependency_links=[
-        "git+https://github.com/HazardDede/argresolver.git@v0.3.0#egg=argresolver-0.3.0"
+        "git+https://github.com/HazardDede/argresolver.git@v0.2.0#egg=argresolver-0.2.0"
     ],
     python_requires='>=3.4',
     include_package_data=True,
