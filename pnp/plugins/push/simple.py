0be74e6e045ae77dc05815f792862d9db7f40ab2
Dennis Muth <dennis.muth@magicline.de>
2018-05-25 07:43:02 +0200

    Documents Echo plugin

--- pnp/plugins/push/simple.py
+++ pnp/plugins/push/simple.py
@@ -2,6 +2,14 @@ from . import PushBase
 
 
 class Echo(PushBase):
+    """
+    Simply logs the payload.
+
+    Examples:
+
+        >>> dut = Echo(name="echo_push")
+        >>> dut.push("I will be logged")
+    """
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
