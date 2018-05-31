79f9f25e4d4b6a1c1d725f302ab8e3ade0fb7b2a
Dennis Muth <dennis.muth@magicline.de>
2018-05-31 20:36:20 +0200

    Introduces some validator method to validate arguments
    Basically for consistent error messages and often used
    validation cases

---
+++ pnp/validator.py
@@ -0,0 +1,127 @@
+import os
+
+from .utils import make_list
+
+
+class Validator:
+    @staticmethod
+    def _allow_none(arg_value, allow_none):
+        return not (allow_none and arg_value is None)
+
+    @staticmethod
+    def is_not_none(**kwargs):
+        """
+        Examples:
+
+            >>> Validator.is_not_none(arg1="foo")  # Ok
+            >>> Validator.is_not_none(arg1=None)  # None
+            Traceback (most recent call last):
+            ...
+            ValueError: Argument 'arg1' is expected to be not none
+        """
+        for arg_name, arg_value in kwargs.items():
+            if arg_value is None:
+                raise ValueError("Argument '{arg_name}' is expected to be not none".format(**locals()))
+
+    @staticmethod
+    def one_not_none(**kwargs):
+        """
+        Examples:
+            >>> Validator.one_not_none(arg1=None, arg2=None, arg3='passed')
+            >>> Validator.one_not_none(arg1=None, arg2=None, arg3=None)
+            Traceback (most recent call last):
+            ...
+            ValueError: Arguments ['arg1', 'arg2', 'arg3'] expects at least one passed, but all are none
+        """
+        if all([x is None for x in kwargs.values()]):
+            raise ValueError("Arguments {args} expects at least one passed, but all are none"
+                             .format(args=sorted(list(kwargs.keys()))))
+
+    @staticmethod
+    def is_instance(*required_type, allow_none=False, **kwargs):
+        """
+        Examples:
+
+            >>> Validator.is_instance(str, arg="i am a string") #  Should be ok
+            >>> Validator.is_instance(str, allow_none=True, arg=None)  # Should be ok as well
+            >>> Validator.is_instance(bool, arg="i am a string")  # Not ok
+            Traceback (most recent call last):
+            ...
+            TypeError: Argument 'arg' is expected to be a (<class 'bool'>,), but is <class 'str'>
+            >>> Validator.is_instance(bool, str, arg="i am a string")  # Should be ok, too
+            >>> Validator.is_instance(str, bool, arg1="i am a string", arg2=True)  # Ok ...
+            >>> Validator.is_instance(str, arg1="i am a string", arg2=True)  # Not ok
+            Traceback (most recent call last):
+            ...
+            TypeError: Argument 'arg2' is expected to be a (<class 'str'>,), but is <class 'bool'>
+        """
+        for arg_name, arg_value in kwargs.items():
+            if Validator._allow_none(arg_value, allow_none) and not isinstance(arg_value, required_type):
+                raise TypeError("Argument '{arg_name}' is expected to be a {required_type}"
+                                ", but is {actual_type}".format(**locals(), actual_type=type(arg_value)))
+
+    @staticmethod
+    def one_of(possible, allow_none=False, **kwargs):
+        """
+        Examples:
+            >>> Validator.one_of(['a', 'b', 'c'], arg='a')
+            >>> Validator.one_of(['a', 'b', 'c'], allow_none=True, arg='a', arg2=None)  # Should be ok as well
+            >>> Validator.one_of(['a', 'b', 'c'], arg='z')
+            Traceback (most recent call last):
+            ...
+            ValueError: Argument 'arg' is expected to be one of ['a', 'b', 'c'], but is 'z'
+            >>> Validator.one_of(['a', 'b', 'c'], arg1='a', arg2='z')
+            Traceback (most recent call last):
+            ...
+            ValueError: Argument 'arg2' is expected to be one of ['a', 'b', 'c'], but is 'z'
+        """
+        Validator.is_instance(list, tuple, possible=possible)
+        for arg_name, arg_value in kwargs.items():
+            if Validator._allow_none(arg_value, allow_none) and arg_value not in possible:
+                raise ValueError("Argument '{arg_name}' is expected to be one of {possible}, "
+                                 "but is '{arg_value}'".format(**locals()))
+
+    @staticmethod
+    def subset_of(possible, allow_none=False, **kwargs):
+        """
+        Examples:
+
+            >>> Validator.subset_of(['a', 'b', 'c'], arg='a')
+            >>> Validator.subset_of(['a', 'b', 'c'], arg=('a', 'b'))
+            >>> Validator.subset_of(['a', 'b', 'c'], arg=('a', 'b'), arg2='c')
+            >>> Validator.subset_of(['a', 'b', 'c'], arg='d')
+            Traceback (most recent call last):
+            ...
+            ValueError: Argument 'arg' is expected to be a subset of ['a', 'b', 'c'], but is 'd'
+            >>> Validator.subset_of(['a', 'b', 'c'], allow_none=True, arg1=None, arg2=None)
+        """
+        Validator.is_instance(list, tuple, possible=possible)
+        for arg_name, arg_value in kwargs.items():
+            if Validator._allow_none(arg_value, allow_none) and not (set(make_list(arg_value)) <= set(possible)):
+                raise ValueError("Argument '{arg_name}' is expected to be a subset of {possible}, but is '{arg_value}'"
+                                 .format(**locals()))
+
+    @staticmethod
+    def is_directory(allow_none=False, **kwargs):
+        """
+        Examples:
+            >>> Validator.is_directory(arg='/tmp')
+            >>> Validator.is_directory(arg='/thisonedoesnotexists')
+            Traceback (most recent call last):
+            ...
+            ValueError: Argument 'arg' is expected to be a directory, but is '/thisonedoesnotexists'
+        """
+        for arg_name, arg_value in kwargs.items():
+            if Validator._allow_none(arg_value, allow_none) and not os.path.isdir(arg_value):
+                raise ValueError("Argument '{arg_name}' is expected to be a directory, "
+                                 "but is '{arg_value}'".format(**locals()))
+
+    @staticmethod
+    def is_file(allow_none=False, **kwargs):
+        """
+
+        """
+        for arg_name, arg_value in kwargs.items():
+            if Validator._allow_none(arg_value, allow_none) and not os.path.isfile(arg_value):
+                raise ValueError("Argument '{arg_name}' is expected to be a file, "
+                                 "but is '{arg_value}'".format(**locals()))
