95a17ce8c801abb8df30fa3d312569dafe7e3d84
Dennis Muth <dennis.muth@magicline.de>
2018-06-02 12:27:22 +0200

    Fixes missinf comma in setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     },
     packages=find_packages(exclude=[]),
     install_requires=[
-        'binaryornot>=0.4.4'
+        'binaryornot>=0.4.4',
         'python-box>=3.2.0',
         'influxdb>=5.0.0',
         'paho-mqtt>=1.3.1',
