f9afee52036681990bfe08030dd3b68bd3c9942a
Dennis Muth <d.muth@gmx.net>
2018-10-04 19:49:16 +0200

    Adds feature to specify logging configuration
    ... whether by envvar `PNP_LOG_CONF`, by passing --log=<log_conf>
    as an argument to pnp runner or by simply drop a logging.yaml
    into the same directory where you run pnp. If no logging configuration
    is specified a basic configuration (backwards compatibility) will be
    used.

--- pnp/runner/pnp.py
+++ pnp/runner/pnp.py
@@ -1,27 +1,43 @@
 """Pull 'n' Push
 
 Usage:
-  pnp [(-c | --check)] <configuration>
+  pnp [(-c | --check)] [--log=<log_conf>] <configuration>
   pnp (-h | --help)
   pnp (-v | --version)
 
 Options:
   -c --check        Only check configuration and do not run it.
+  --log=<log_conf>  Specify logging configuration to load.
   -h --help         Show this screen.
   -v --version      Show version.
 
 """
 
 import logging
+import logging.config
 import os
 
 from docopt import docopt
-from schema import Schema, Use, And
+from schema import Schema, Use, And, Or
+from ruamel import yaml
 
 from ..app import Application
 
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=LOG_LEVEL)
+
+def setup_logging(default_path, default_level=logging.INFO, env_key='PNP_LOG_CONF'):
+    """Setup logging configuration"""
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+        logging.info("Logging loaded from: {}".format(path))
+    else:
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=default_level)
+        logging.info("Logging loaded with basic configuration")
 
 
 def validate_args(args):
@@ -29,12 +45,15 @@ def validate_args(args):
         "--help": Use(bool),
         "--version": Use(str),
         "--check": Use(bool),
+        "--log": Or(None, And(os.path.isfile, Use(os.path.abspath))),
         "<configuration>": And(os.path.isfile, Use(os.path.abspath))
     })
     return validator.validate(args)
 
 
 def run(args):
+    default_log_level = os.environ.get('LOG_LEVEL', 'DEBUG')
+    setup_logging(default_path=(args['--log'] or 'logging.yaml'), default_level=default_log_level)
     validated = validate_args(args)
     app = Application.from_file(validated['<configuration>'])
     if not validated['--check']:
