4b849c3ca2537b105b0c84f7919be12ab6ac7484
Dennis Muth <d.muth@gmx.net>
2018-11-27 15:24:26 +0100

    Adds --verbose option to pnp runner

--- pnp/runner/pnp.py
+++ pnp/runner/pnp.py
@@ -1,15 +1,16 @@
 """Pull 'n' Push
 
 Usage:
-  pnp [(-c | --check)] [--log=<log_conf>] <configuration>
+  pnp [(-c | --check)] [(-v | --verbose)] [--log=<log_conf>] <configuration>
   pnp (-h | --help)
-  pnp (-v | --version)
+  pnp --version
 
 Options:
   -c --check        Only check configuration and do not run it.
+  -v --verbose      Switches log level to debug.
   --log=<log_conf>  Specify logging configuration to load.
   -h --help         Show this screen.
-  -v --version      Show version.
+  --version         Show version.
 
 """
 
@@ -25,7 +26,7 @@ from ..utils import get_first_existing_file
 from ..app import Application
 
 
-def setup_logging(*candidates, default_level=logging.INFO, env_key='PNP_LOG_CONF'):
+def setup_logging(*candidates, default_level=logging.INFO, env_key='PNP_LOG_CONF', verbose=False):
     """Setup logging configuration"""
     log_file_path = get_first_existing_file(*candidates)
     env_path = os.getenv(env_key, None)
@@ -36,14 +37,18 @@ def setup_logging(*candidates, default_level=logging.INFO, env_key='PNP_LOG_CONF
             config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
         logging.info("Logging loaded from: {}".format(log_file_path))
+        if verbose:
+            logging.getLogger().setLevel(logging.DEBUG)
     else:
-        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=default_level)
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.DEBUG if verbose else default_level)
         logging.info("Logging loaded with basic configuration")
 
 
 def validate_args(args):
     validator = Schema({
         "--help": Use(bool),
+        "--verbose": Use(bool),
         "--version": Use(str),
         "--check": Use(bool),
         "--log": Or(None, And(os.path.isfile, Use(os.path.abspath))),
@@ -57,7 +62,7 @@ def run(args):
     pnp_cfg_path = validated['<configuration>']
     default_log_level = os.environ.get('LOG_LEVEL', 'DEBUG')
     setup_logging(args['--log'], 'logging.yaml', os.path.join(os.path.dirname(pnp_cfg_path), 'logging.yaml'),
-                  default_level=default_log_level)
+                  default_level=default_log_level, verbose=validated['--verbose'])
     app = Application.from_file(pnp_cfg_path)
     if not validated['--check']:
         app.start()
