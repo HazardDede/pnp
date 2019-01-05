9bc5f23d804abbe054ff0717d215ee7419e29bf0
Dennis Muth <d.muth@gmx.net>
2019-01-05 11:50:16 +0100

    Adds MQTT Discovery push

--- pnp/plugins/push/mqtt.py
+++ pnp/plugins/push/mqtt.py
@@ -1,10 +1,68 @@
 import json
+from abc import abstractmethod
+
+from dictmentor import DictMentor, ext
 
 from . import PushBase
-from ...utils import try_parse_bool, try_parse_int
+from ...utils import try_parse_bool, try_parse_int, auto_str_ignore
+from ...validator import Validator
 
 
-class Publish(PushBase):
+@auto_str_ignore(['password'])
+class _MQTTBase(PushBase):
+    __prefix__ = 'mqtt'
+
+    def __init__(self, host, port=1883, user=None, password=None, qos=0, **kwargs):
+        super().__init__(**kwargs)
+        self.host = str(host)
+        self.port = int(port)
+        self.user = user and str(user)
+        self.password = password and str(password)
+        self.qos = self._parse_qos(qos)
+
+    def _parse_qos(self, val):
+        pval = try_parse_int(val)
+        if pval is None:
+            self.logger.warning("QOS level of '{val}' is not int parsable. Defaulting to 0".format(**locals()))
+            return 0
+        if pval < 0:
+            self.logger.warning("QOS level of '{val}' is invalid. Defaulting to 0".format(**locals()))
+            return 0
+        if pval > 2:
+            self.logger.warning("QOS level of '{val}' is invalid. Defaulting to 2".format(**locals()))
+            return 2
+        return pval
+
+    def _publish(self, real_payload, topic, retain=False, qos=None):
+        if isinstance(real_payload, (dict, list, tuple)):
+            real_payload = json.dumps(real_payload)
+
+        auth = None
+        if self.user:
+            auth = dict(username=self.user, password=self.password)
+
+        if qos is None:
+            qos = self.qos
+
+        import paho.mqtt.publish as publish
+        publish.single(
+            topic=topic,
+            payload=real_payload,
+            hostname=self.host,
+            port=self.port,
+            retain=retain,
+            auth=auth,
+            qos=qos
+        )
+        self.logger.debug("[{self.name}] Published message on '{topic}' @ {self.host}:{self.port} with qos={qos}. "
+                          "Payload='{real_payload}'".format(**locals()))
+
+    @abstractmethod
+    def push(self, payload):
+        raise NotImplementedError()
+
+
+class Publish(_MQTTBase):
     """
     This push will push the given `payload` to a mqtt broker (in this case mosquitto).
     The broker is specified by `host` and `port`. In addition a topic needs to be specified were the payload
@@ -12,13 +70,6 @@ class Publish(PushBase):
 
     The `payload` will be pushed as it is. No transformation is applied. If you need to some transformations, use the
     selector.
-
-    Args:
-        host (str): The host where the mosquitto broker is running.
-        port (int): The port where the mosquitto broker is listening (default is 1883).
-        topic (str): The topic to subscribe to. If set to None the envelope of the
-                     payload has to contain a topic key or the push will fail (default is None). If both exists
-                     the topic from the envelope will overrule the __init__ one.
 
     Returns:
         For chaining of pushes the payload is simply returned as is.
@@ -35,57 +86,18 @@ class Publish(PushBase):
             topic: home/devices/#
             port: 1883
     """
-    __prefix__ = 'mqtt'
 
-    def __init__(self, host, topic=None, port=1883, retain=False, user=None, password=None, qos=0,
-                 multi=False, list_item_push=False, **kwargs):
+    def __init__(self, topic=None, retain=False, multi=False, **kwargs):
         super().__init__(**kwargs)
-        self.host = str(host)
         self.topic = self._parse_topic(topic)
-        self.port = int(port)
         self.retain = self._parse_retain(retain)
-        self.user = user and str(user)
-        self.password = password and str(password)
-        self.qos = self._parse_qos(qos)
         self.multi = bool(multi)
-        self.list_item_push = bool(list_item_push)
 
     def _parse_retain(self, val):
         return try_parse_bool(val, default=False)
 
     def _parse_topic(self, val):
         return str(val) if val is not None else None
-
-    def _parse_qos(self, val):
-        pval = try_parse_int(val)
-        if pval is None:
-            self.logger.warning("QOS level of '{val}' is not int parsable. Defaulting to 0".format(**locals()))
-            return 0
-        if pval < 0:
-            self.logger.warning("QOS level of '{val}' is invalid. Defaulting to 0".format(**locals()))
-            return 0
-        if pval > 2:
-            self.logger.warning("QOS level of '{val}' is invalid. Defaulting to 2".format(**locals()))
-            return 2
-        return pval
-
-    def _publish(self, auth, qos, real_payload, retain, topic):
-        import paho.mqtt.publish as publish
-
-        if isinstance(real_payload, (dict, list, tuple)):
-            real_payload = json.dumps(real_payload)
-
-        publish.single(
-            topic=topic,
-            payload=real_payload,
-            hostname=self.host,
-            port=self.port,
-            retain=retain,
-            auth=auth,
-            qos=qos
-        )
-        self.logger.debug("[{self.name}] Published message on '{topic}' @ {self.host}:{self.port} with qos={qos}. "
-                          "Payload='{real_payload}'".format(**locals()))
 
     @staticmethod
     def _topic_concat(t1, t2):
@@ -102,18 +114,15 @@ class Publish(PushBase):
         if topic is None:
             raise ValueError("Topic was not defined either by the __init__ nor by the envelope")
 
-        auth = None
-        if self.user:
-            auth = dict(username=self.user, password=self.password)
         if not self.multi:
-            self._publish(auth, qos, real_payload, retain, topic)
+            self._publish(real_payload, topic, retain, qos)
         else:
             if not isinstance(real_payload, dict):
                 raise TypeError("In multi mode the payload is required to be a dictionary")
             for k, v in real_payload.items():
                 key_topic = self._topic_concat(topic, k)
                 try:
-                    self._publish(auth, qos, v, retain, key_topic)
+                    self._publish(v, key_topic, retain, qos)
                 except:
                     import traceback
                     ex = traceback.format_exc()
@@ -121,15 +130,78 @@ class Publish(PushBase):
                                       "{self.host}:{self.port} with qos={qos}. Payload='{v}'\n{ex}".format(**locals()))
 
     def push(self, payload):
-        if self.list_item_push:
-            if not isinstance(payload, list):
-                raise TypeError("In multi_push mode the payload is required to be a list")
-            for msg in payload:
-                self._push_single(msg)
-        else:
-            self._push_single(payload)
-
+        self._push_single(payload)
         return payload  # Payload as is. With envelope (if any).
 
 
 MQTTPush = Publish
+
+
+@auto_str_ignore(['configured'])
+class Discovery(_MQTTBase):
+    SUPPORTED_COMPONENTS = ['alarm_control_panel', 'binary_sensor', 'camera', 'cover', 'fan', 'climate', 'light',
+                            'lock', 'sensor', 'switch']
+
+    def __init__(self, discovery_prefix, component, config, object_id=None, node_id=None, **kwargs):
+        super().__init__(**kwargs)
+        self.discovery_prefix = str(discovery_prefix)
+        Validator.is_instance(str, component=component)
+        Validator.one_of(self.SUPPORTED_COMPONENTS, component=component)
+        self.component = component
+        self.object_id = self._parse_object_id(object_id)
+        Validator.is_instance(dict, config=config)
+        self._config = config
+        self.node_id = self._parse_node_id(node_id)
+        self.configured = {}
+
+    @property
+    def config(self):
+        import copy
+        return copy.copy(self._config)
+
+    def _parse_object_id(self, val):
+        return str(val)
+
+    def _parse_node_id(self, val):
+        return val and str(val)
+
+    def _topics(self, object_id, node_id):
+        node_id = "{node_id}/".format(node_id=node_id) if node_id else ""
+        base_topic = "{self.discovery_prefix}/{self.component}/{node_id}{object_id}".format(**locals())
+        state_topic = base_topic + "/state"
+        config_topic = base_topic + "/config"
+        return base_topic, config_topic, state_topic
+
+    def _configure(self, object_id, node_id):
+        configure_key = str(object_id) + str(node_id)
+        if configure_key not in self.configured:
+            base_topic, config_topic, state_topic = self._topics(object_id, node_id)
+            mentor = DictMentor(
+                ext.Variables(
+                    fail_on_unset=True,
+                    discovery_prefix=self.discovery_prefix,
+                    component=self.component,
+                    object_id=object_id,
+                    node_id=node_id,
+                    base_topic=base_topic,
+                    config_topic=config_topic,
+                    state_topic=state_topic
+                )
+            )
+            config_augmented = mentor.augment(self.config)
+            self._publish(config_augmented, config_topic, retain=True)
+            self.configured[configure_key] = True
+
+    def push(self, payload):
+        envelope, real_payload = self.envelope_payload(payload)
+        object_id = self._parse_envelope_value('object_id', envelope)  # Override object_id via envelope
+        node_id = self._parse_envelope_value('node_id', envelope)  # Override node_id via envelope
+
+        if object_id is None:
+            raise ValueError("object_id was not defined either by the __init__ nor by the envelope")
+
+        self._configure(object_id, node_id)
+        _, _, state_topic = self._topics(object_id, node_id)
+        self._publish(real_payload, state_topic, retain=True)
+
+        return payload
