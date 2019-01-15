76674d0ca634f133655282a4ab357199e9be8abd
Dennis Muth <d.muth@gmx.net>
2019-01-15 20:27:33 +0100

    Adds unit tests for push.mqtt.Discovery plugin

---
+++ tests/plugins/push/test_mqtt_discovery.py
@@ -0,0 +1,91 @@
+import json
+
+from pnp.plugins.push.mqtt import Discovery
+
+
+def test_push_mqtt_discovery(monkeypatch):
+    call_cnt = 0
+    calls = []
+    def call_validator(*args, **kwargs):
+        nonlocal call_cnt
+        call_cnt += 1
+        calls.append(kwargs)
+
+    import paho.mqtt.publish
+    monkeypatch.setattr(paho.mqtt.publish, 'single', call_validator)
+
+    config = {'friendly_name': 'pytest_sensor'}
+    dut = Discovery(
+        host='doesnotmatter',
+        discovery_prefix='pytest',
+        component='sensor',
+        config=config,
+        object_id='pytest_sensor',
+        node_id='12345',
+        name='pytest__push'
+    )
+    dut.push(10)
+
+    assert call_cnt == 2
+    # config
+    assert calls[0] == {'topic': 'pytest/sensor/12345/pytest_sensor/config', 'payload': '{"friendly_name": "pytest_sensor"}', 'hostname': 'doesnotmatter', 'port': 1883, 'retain': True, 'auth': None, 'qos': 0}
+    # state
+    assert calls[1] == {'topic': 'pytest/sensor/12345/pytest_sensor/state', 'payload': 10, 'hostname': 'doesnotmatter', 'port': 1883, 'retain': True, 'auth': None, 'qos': 0}
+
+
+def test_push_mqtt_discovery_envelope_override(monkeypatch):
+    call_cnt = 0
+    calls = []
+    def call_validator(*args, **kwargs):
+        nonlocal call_cnt
+        call_cnt += 1
+        calls.append(kwargs)
+
+    import paho.mqtt.publish
+    monkeypatch.setattr(paho.mqtt.publish, 'single', call_validator)
+
+    config = {'friendly_name': 'pytest_sensor'}
+    dut = Discovery(
+        host='doesnotmatter',
+        discovery_prefix='pytest',
+        component='sensor',
+        config=config,
+        object_id='pytest_sensor',
+        node_id='12345',
+        name='pytest__push',
+    )
+    dut.push({'data': 10, 'object_id': 'object_override', 'node_id': 'node_override'})
+
+    assert call_cnt == 2
+    # config
+    assert calls[0] == {'topic': 'pytest/sensor/node_override/object_override/config', 'payload': '{"friendly_name": "pytest_sensor"}', 'hostname': 'doesnotmatter', 'port': 1883, 'retain': True, 'auth': None, 'qos': 0}
+    # state
+    assert calls[1] == {'topic': 'pytest/sensor/node_override/object_override/state', 'payload': 10, 'hostname': 'doesnotmatter', 'port': 1883, 'retain': True, 'auth': None, 'qos': 0}
+
+
+def test_push_mqtt_config_vars(monkeypatch):
+    call_cnt = 0
+    calls = []
+    def call_validator(*args, **kwargs):
+        nonlocal call_cnt
+        call_cnt += 1
+        calls.append(kwargs)
+
+    import paho.mqtt.publish
+    monkeypatch.setattr(paho.mqtt.publish, 'single', call_validator)
+
+    config = {'friendly_name': 'pytest_sensor', 'object_id': '{{var::object_id}}', 'node_id': '{{var::node_id}}'}
+    dut = Discovery(
+        host='doesnotmatter',
+        discovery_prefix='pytest',
+        component='sensor',
+        config=config,
+        object_id='pytest_sensor',
+        node_id='12345',
+        name='pytest__push',
+    )
+    dut.push(10)
+
+    assert call_cnt == 2
+    # config
+    assert json.loads(calls[0]['payload']) == {'friendly_name': 'pytest_sensor', 'object_id': 'pytest_sensor', 'node_id': '12345'}
