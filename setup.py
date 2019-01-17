89c027ca577073378605644d1939179f79bb7906
Dennis Muth <d.muth@gmx.net>
2019-01-17 20:55:37 +0100

    Adds missing dependency `attrs` to setup.py

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'argresolver>=0.3.2',
         'asyncio>=3.4.0',
         'asyncws>=0.1',
+        'attrs>=18.0.0',
         'binaryornot>=0.4.4',
         'dictmentor>=0.2.0',
         'docopt>=0.6.2',
