00d495fe9f53724628efcac5acd4031525f17622
Dennis Muth <d.muth@gmx.net>
2020-02-28 20:43:03 +0100

    Only install fritz extra on python >= 3.6

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
+
+echo Python Version ${TRAVIS_PYTHON_VERSION}
+
+if [ "${TRAVIS_PYTHON_VERSION}" != '3.5' ]; then
+    pip install -e .[fritz]
+fi
+
 if [ "${TRAVIS_EVENT_TYPE:-none}" == 'cron' ]; then
     pip install -e .[faceR]
 fi
