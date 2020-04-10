77ccc05c450fed5c661655ba1a57c29ac8fb4dc3
HazardDede <d.muth@gmx.net>
2020-04-10 20:47:58 +0200

    Feature/ssl verify (#15)
    * Implements `pull.net.SSLVerify` to check ssl certificates

--- docs/plugins/pull/README.md
+++ docs/plugins/pull/README.md
@@ -961,6 +961,44 @@ __Examples__
     - plugin: pnp.plugins.push.simple.Echo
 
 ```
+## pnp.plugins.pull.net.SSLVerify
+
+Periodically checks if the ssl certificate of a given host is valid and how
+many days are remaining before the certificate will expire.
+
+__Arguments__
+
+- **host (str)**: The host
+- **timeout (float, optional)**: Timeout for remote checking. Default is 3 seconds.
+
+__Result__
+
+```python
+{
+    # Envelope
+    "host": "www.google.de",
+    "payload": {
+        "expires_days": 50,  # Remaining days before expiration
+        "expires_at": datetime.datetime(2020, 5, 26, 9, 45, 52),  # Python datetime of expiration
+        "expired": False  # True of the certificate is expired; otherwise False.
+    }
+}
+```
+
+__Examples__
+
+```yaml
+- name: ssl_verify
+  pull:
+    plugin: pnp.plugins.pull.net.SSLVerify
+    args:
+      host: www.google.com  # Check the ssl certificate for this host
+      interval: 1m  # Check the ssl certificate every minute
+      instant_run: true  # ... and run as soon as pnp starts
+  push:
+    - plugin: pnp.plugins.push.simple.Echo
+
+```
 ## pnp.plugins.pull.presence.FritzBoxTracker
 
 Periodically asks a Fritz!Box router for the devices that were connected in the past or right now.
--- pnp/plugins/pull/net.py
+++ pnp/plugins/pull/net.py
@@ -1,9 +1,13 @@
 """Network related plugins."""
 
 import socket
+import ssl
+from datetime import datetime, timedelta
+from typing import Tuple
 
 from . import Polling
 from ...utils import is_local
+from ...typing import Payload
 
 
 class PortProbe(Polling):
@@ -63,3 +67,57 @@ class PortProbe(Polling):
             return False
         except socket.error:
             return True
+
+
+class SSLVerify(Polling):
+    """
+    Periodically checks if the ssl certificate of a given host is valid and how
+    many days are remaining before the certificate will expire.
+
+    See Also:
+        https://github.com/HazardDede/pnp/blob/master/docs/plugins/pull/net.SSLVerify/index.md
+    """
+
+    def __init__(self, host: str, timeout: float = 3.0, **kwargs):
+        super().__init__(**kwargs)
+        self.host = str(host)
+        self.timeout = float(timeout)
+
+    def _ssl_expiry_datetime(self) -> Tuple[datetime, timedelta]:
+        """
+        Fetches the expiration date of the ssl certificate.
+
+        Returns:
+            Tuple that contains the timestamp the certificate will expire
+            and the number of days left.
+        """
+        ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+
+        context = ssl.create_default_context()
+        conn = context.wrap_socket(
+            socket.socket(socket.AF_INET),
+            server_hostname=self.host,
+        )
+        conn.settimeout(self.timeout)
+
+        conn.connect((self.host, 443))
+        self.logger.debug("SSL Host: %s", self.host)
+        ssl_info = conn.getpeercert()
+        self.logger.debug("Connection to host %s successful", self.host)
+
+        # parse the string from the certificate into a Python datetime object
+        expires = datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+        self.logger.debug('SSL certificate for %s expires at %s', self.host, expires.isoformat())
+
+        return expires, expires - datetime.utcnow()
+
+    def poll(self) -> Payload:
+        expires_at, expires_delta = self._ssl_expiry_datetime()
+        return {
+            'host': self.host,
+            'payload': {
+                'expires_days': expires_delta.days,
+                'expires_at': expires_at,
+                'expired': expires_delta <= timedelta(days=0)
+            }
+        }
