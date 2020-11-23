a2d3d891c49f476a745a50e5eff2daf9ba6366e5
Dennis <d.muth@gmx.net>
2020-11-23 11:50:58 +0100

    Adds archive_name argument to push.fs.Zipper (#31)
    archive_name allows to control the name of the resulting zip archive.
    It is useful for tasks where you want to change the archive name
    at runtime (like adding a date to it).
    See the examples on how to do it

--- tests/plugins/push/test_fs_zipper.py
+++ tests/plugins/push/test_fs_zipper.py
@@ -39,3 +39,21 @@ def test_zipper_push_payload_file():
         zip_file_name = dut.push(path)
         assert zip_file_name == os.path.join(tmpdir, '1' + '.zip')
         assert set(ZipFile(zip_file_name).namelist()) == {'1'}
+
+
+def test_zipper_push_archive_name():
+    path = os.path.join(os.path.dirname(__file__), '../../resources/zipping/testdir')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dut = Zipper(name='pytest', out_path=tmpdir, archive_name='foo.zip')
+        zip_file_name = dut.push(path)
+        assert zip_file_name == os.path.join(tmpdir, 'foo' + '.zip')
+        assert set(ZipFile(zip_file_name).namelist()) == {'1', '2/2'}
+
+
+def test_zipper_push_archive_name_override():
+    path = os.path.join(os.path.dirname(__file__), '../../resources/zipping/testdir')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dut = Zipper(name='pytest', out_path=tmpdir)
+        zip_file_name = dut.push({'data': path, 'archive_name': 'foo.zip'})
+        assert zip_file_name == os.path.join(tmpdir, 'foo' + '.zip')
+        assert set(ZipFile(zip_file_name).namelist()) == {'1', '2/2'}
